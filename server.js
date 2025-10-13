50468f4a66508f903c658d5f6f7445e1b4c4dbb0
Janire <janiremartinez1997@gmail.com>
2025-10-13 19:53:22 +0200

    app.cors update

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: [ "http://localhost:5173", "http://127.0.0.1:5173" ],
+    origin: [ 
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173",
+        "https://jmg-final-project-production.up.railway.app" 
+    ],
     credentials: true
 }));
 
